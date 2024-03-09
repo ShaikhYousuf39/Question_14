0df10c53ba9be0a74481a47144eb4e07f37d7e45
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:10:46 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,2 @@
+var guestsList = ["Mehwish", "Rohan", "Ali"];
+guestsList.map(function (items) { return console.log("Dear ".concat(items, ", You Are In Invited In My House For Dinner. ")); });
---
+++ new.ts
@@ -0,0 +1,3 @@
+let guestsList: string[] = ["Mehwish","Rohan","Ali"];
+
+guestsList.map((items) => console.log(`Dear ${items}, You Are In Invited In My House For Dinner. `));
